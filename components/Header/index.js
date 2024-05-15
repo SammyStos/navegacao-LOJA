60d241a648e8e5fcefa080b85fdf080084f924c9
Samantha Paula <you@example.com>
2024-05-15 16:17:18 -0300

    Nossa Rotina Semanal

--- components/Header/index.js
+++ components/Header/index.js
@@ -4,8 +4,9 @@ import estilo from './style'
 export default function Header(){
     return(
         <>
-            <Text style={estilo.titulo}>Minha Rotina Semanal</Text>
-           
+            <Text style={estilo.titulo}>Nossa Rotina Semanal</Text>
+            <Text style={estilo.subtitulo}>Total de Horas = 168 - 100%</Text>
+            <Text style={estilo.subtitulo}>Rafa e Sammy</Text>
              <View style={{width:'95%', flexDirection: 'row', marginTop: 20, alignItems:'center', justifyContent:"space-between" }}>
             <Text style={estilo.tituloCard}>Atividade</Text>
             <Text style={estilo.tituloCard}>Descrição</Text>
---
+++ data/index.js
@@ -0,0 +1,47 @@
+let DATA =  [
+    {
+        id: '01',
+        porcentagem: '16%', 
+        titulo: 'Assistir Alanzoka',
+        imagem: 'https://i.pinimg.com/474x/56/c9/ca/56c9ca65b23712ec0eaa8450f07e5a28.jpg',
+        descricao: 'Amo assistir Alanzoka, quando chego da Etec, ainda mais se for almoçando' 
+    },
+    {
+        id: '02',
+        porcentagem: '16%', 
+        titulo: 'Comer',
+        imagem: 'https://s2-receitas.glbimg.com/1EvgesvUMInKftWfkHKQKf6D9FI=/0x0:1280x800/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_1f540e0b94d8437dbbc39d567a1dee68/internal_photos/bs/2022/8/O/xH9h1GSnW1LhMobyL7hQ/strogonoff-de-frango-receita.jpg',
+        descricao: 'Amo comer um pratão quando chego da Etec, principalmente se for Strogonoff' 
+    },
+    {
+        id: '03',
+        porcentagem: '16%', 
+        titulo: 'Mimir',
+        imagem: 'https://s2-vidadebicho.glbimg.com/VzTonKa6N1PhtLkQInss22tUhww=/0x0:960x640/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_fb623579cd474803aedbbbbae014af68/internal_photos/bs/2022/C/q/rSqQiHQuOjeBeckevESw/2022-01-27-vidadebicho-gato-dormindo-tabby-cat-6788375-960-720.jpeg',
+        descricao: 'Amo mimir quando chego da Etec, ou quando estou somente em casa (Acordo somente no dia seguinte)' 
+    },
+    {
+        id: '04',
+        porcentagem: '16%', 
+        titulo: 'Lições da Etec',
+        imagem: 'https://bkpsitecpsnew.blob.core.windows.net/uploadsitecps/sites/1/2020/10/62649497_1099632600224745_1950717718562865152_n.jpg',
+        descricao: 'Fico triste quando tem lição para fazer, mas tenho que fazer para tirar nota boa' 
+    },
+    {
+        id: '05',
+        porcentagem: '16%', 
+        titulo: 'Ouvir Musica',
+        imagem: 'https://www.billboard.com/wp-content/uploads/2023/12/taylor-swift-eras-foxborough-2023-billboard-1548.jpg',
+        descricao: 'Amo escutar musica, ainda mais se for da loirinha' 
+    },
+    {
+        id: '06',
+        porcentagem: '16%', 
+        titulo: 'Ler livros',
+        imagem: 'https://play-lh.googleusercontent.com/Y244hl0BV8-nfkNJC5v_8awOsD0tN2emx-O1ZIzGrMdO9rgXQFB0AACoSczu0NAxKhY',
+        descricao: 'Amo ler, qualquer tipo de livros, ainda mais se for da biblia laranja' 
+    }
+   
+]
+
+export default DATA;
